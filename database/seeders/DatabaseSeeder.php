0f9e5279cfec0a072b667b3df49d45a876e0bf26
hiep1998vnhn11 <hiep1998vnhn11@gmail.com>
2021-05-27 23:25:35 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        $this->call(UserSeeder::class);
         $this->call(DanhMucSeeder::class);
         $this->call(ThuTucSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
